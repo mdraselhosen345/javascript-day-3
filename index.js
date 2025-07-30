7134762bc3d4811cb319071673ae914dc7dc1d9e
md rasel islam <rashel099009@gamil.com>
2025-07-30 15:35:25 +0600

    added index.js

--- index.js
+++ index.js
@@ -45,10 +45,41 @@
 // }
 // console.log("sum of odd numbers from 1 to 20 is :" +sum);
 
-let sum = 0;
-for(let i=51; i<=85; i++){
-    if(i%2 === 0){
-        sum +=i
+// let sum = 0;
+// for(let i=51; i<=85; i++){
+//     if(i%2 === 0){
+//         sum +=i
+//     }
+// }
+// console.log("sum of odd numbers from 51 to 85 is:"+sum);
+
+
+// Generate a multiplication table for number 1
+// let number = 1;
+// for(let i=1; i<=10; i++){
+//     console.log(number + "x"+i+"="+(number*i));
+// }
+// let number = 9;
+// for(let i=1; i<=10; i++){
+//     console.log(number+"x"+i+"="+number*i);
+// }
+// Implement a countdown timer that counts down from 100 to 1;
+
+// let count = 100;
+// let timer = setInterval(function(){
+//     console.log(count);
+//     count--;
+//     if(count < 1){
+//         clearInterval(timer);
+//     }
+// },1000);
+    
+let count = 1;
+let timer = setInterval(function(){
+    console.log(count);
+    count++;
+    if(count > 100){
+        clearInterval(timer);
     }
-}
-console.log("sum of odd numbers from 51 to 85 is:"+sum);
+},1000);
+    

a5d5b56287de3f8b36cc6a121b452b1d60c43290
md rasel islam <rashel099009@gamil.com>
2025-07-30 14:54:07 +0600

    added the index.js

--- index.js
+++ index.js
@@ -1,4 +1,54 @@
 //--------1---------
-for(let i=1; i<=60; i++){
-    console.log("daily 6 hour study");
+// for(let i=1; i<=60; i++){
+//     console.log("daily 6 hour study");
+// }
+
+
+//    Find all the odd numbers 
+// for(let i= 1; i<=20; i++){
+//     if(i%2 !==0){
+//         console.log(i);
+//     }
+// }
+
+
+// for(let i = 1; i<= 20; i += 2){
+//     console.log(i);
+// }
+// for(let i = 61; i <= 100; i+=2){
+//     console.log(i)
+// }
+
+//Find all the even numbers
+// for(let i = 78; i <= 98; i++){
+//     if(i%2 ===0);
+//     console.log(i)
+// }
+// for(let i = 1; i <= 20; i++){
+//    if(i % 2===0);
+//    console.log(i)
+// }
+
+// let sum = 0;
+
+// for(let i=91; i <=129; i += 2){
+//      sum +=i;
+// }
+// console.log("sum of odd numbers from 91 to 129is:" +sum);
+
+// let sum = 0;
+
+// for(let i=1; i<=20; i++){
+//     if(i % 2 !==0){
+//         sum +=i;
+//     }
+// }
+// console.log("sum of odd numbers from 1 to 20 is :" +sum);
+
+let sum = 0;
+for(let i=51; i<=85; i++){
+    if(i%2 === 0){
+        sum +=i
+    }
 }
+console.log("sum of odd numbers from 51 to 85 is:"+sum);
